556f1f615752d6b8be5c3a751446c3758d760277
ruan <ruan.ns1999@gmail.com>
2025-09-01 02:08:18 -0300

    adição da nova rota de post,criação do arquivo de schemas e novos testes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,38 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'hello world'}
+    return {'message': 'ola mundo'}
+
+
+@app.get('/rota', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def rota2():
+    return """
+    <html>
+        <head>
+            <title>meu ola mundo</title>
+        </head>
+        <body>
+            <h1>Ola mundo</h1>
+        </body>
+    </html>"""
+
+
+database = []
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,44 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'hello world'}
+    assert response.json() == {'message': 'ola mundo'}
+
+
+def test_rota(client):
+    response = client.get('/rota')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+        <head>
+            <title>meu ola mundo</title>
+        </head>
+        <body>
+            <h1>Ola mundo</h1>
+        </body>
+    </html>"""
+    )
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@exemplo.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@exemplo.com',
+        'id': 1,
+    }
